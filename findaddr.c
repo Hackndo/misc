3a3518c4029fb8e9762e261f04e08df79fd717cd
hackndo <hackndo@gmail.com>
2015-06-29 17:55:03 +0200

    Fix source

--- findaddr.c
+++ findaddr.c
@@ -11,12 +11,13 @@
  
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char* argv[])
     {
         if (argc != 2) {
         printf("%s\n", "Usage : findaddr ENV_NAME");
         return 0;
     }
-    printf("%s address: 0x%lx\n",argv[1], getenv(argv[1]));
+    printf("%s address: %p\n",argv[1], (void *)getenv(argv[1]));
     return 0;
 }
