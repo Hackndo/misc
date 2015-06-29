357682e093ef40c608364a85e5143b6e7d480b58
Betezed <bentz.romain@gmail.com>
2015-06-29 18:11:33 +0200

    Add source execshellcode

--- execshellcode.c
+++ execshellcode.c
@@ -6,6 +6,7 @@
  * $ ./execshellcode $(perl -e 'print "<shellcode>"')
  * Example (from http://shell-storm.org/shellcode/files/shellcode-827.php):
  * $ ./execshellcode $(perl -e 'print "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"')
+ * Shellcode Length: 23
  * $
  */
 
