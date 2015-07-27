1e9a461ed2df04158d55775049f0b4eab9e8338f
Betezed <bentz.romain@gmail.com>
2015-07-27 13:56:18 +0200

    Update shellstorm url

--- execshellcode.c
+++ execshellcode.c
@@ -4,7 +4,7 @@
  * $ gcc -Wall -m32 -Wl,-z,execstack -o execshellcode execshellcode.c
  * Usage :
  * $ ./execshellcode <shellcode>
- * Example (from http://shell-storm.org/shellcode/files/shellcode-827.php):
+ * Example (from http://shell-storm.org/shellcode/files/shellcode-811.php):
  * hackndo@hackndo:~ $ ./execshellcode "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
  * Shellcode Length: 28
  * $ 
