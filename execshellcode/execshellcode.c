437bf96c24ee0b7bebc105d1134a23d71dbb633f
Romain <hackndo@gmail.com>
2015-08-10 09:30:36 +0200

    Update execshellcode.c

--- execshellcode/execshellcode.c
+++ execshellcode/execshellcode.c
@@ -1,15 +1,3 @@
-/*
- * Execute given shellcode
- * Compile :
- * $ gcc -Wall -m32 -Wl,-z,execstack -o execshellcode execshellcode.c
- * Usage :
- * $ ./execshellcode <shellcode>
- * Example (from http://shell-storm.org/shellcode/files/shellcode-811.php):
- * hackndo@hackndo:~ $ ./execshellcode "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
- * Shellcode Length: 28
- * $ 
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
