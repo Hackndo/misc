662a666852b2d6d9c7bd483efda09795489c1a10
hackndo <hackndo@gmail.com>
2017-04-13 19:11:13 +0200

    Fix

--- cryde/cryde.py
+++ cryde/cryde.py
@@ -171,6 +171,10 @@ class Cipher(object):
             "64": {
                 "ci": False,
                 "chars": ['/','=','0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            },
+            "morse": {
+                "ci": True,
+                "chars": ["-", "."]
             }
         }
 
@@ -194,6 +198,7 @@ def main():
         Cipher("019cf44a019cf44a019cf44a019cf44a", spaces=False),
         Cipher("--. .--. ---", spaces=False),
         Cipher("JBSWY3DPEB3W64TMMQ======", spaces=False),
+        Cipher("MN_g-q4trIizs6ekvdF93U0s5fY"),
     ]
     for cipher in ciphers:
         print("\n======= NEW CIPHER ========\n")
