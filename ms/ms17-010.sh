08e2353a07894597da6bd26173fb8d68a028e46c
Pixis <hackndo@gmail.com>
2017-05-16 15:48:34 +0200

    Update ms17-010.sh

--- ms/ms17-010.sh
+++ ms/ms17-010.sh
@@ -1,6 +1,5 @@
 # Download NSE script
-cd /usr/share/nmap/scripts
-wget https://raw.githubusercontent.com/cldrn/nmap-nse-scripts/master/scripts/smb-vuln-ms17-010.nse
+wget -P /usr/share/nmap/scripts https://raw.githubusercontent.com/cldrn/nmap-nse-scripts/master/scripts/smb-vuln-ms17-010.nse
 
 # Check vulnerable machines
 nmap -p445 --script smb-vuln-ms17-010 192.168.1.103
