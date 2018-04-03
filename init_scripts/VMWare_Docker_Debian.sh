0fe2694bc3137eb05a3dfb3b858259c57ca1fc70
Pixis <hackndo@gmail.com>
2018-04-03 12:06:28 +0200

    Create VMWare_Docker_Debian.sh

---
+++ init_scripts/VMWare_Docker_Debian.sh
@@ -0,0 +1,22 @@
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+cd /tmp
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install docker-compose
+sudo apt-get install git build-essential dkms gcc linux-headers-$(uname -r)
+mkdir docker
