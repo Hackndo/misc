800f2a9c74be909e1d50a0077d41f0fc4db0e25d
Pixis <hackndo@gmail.com>
2018-08-21 17:04:31 +0200

    Update Install_Docker_Debian.sh

--- init_scripts/Install_Docker_Debian.sh
+++ init_scripts/Install_Docker_Debian.sh
@@ -1,11 +1,6 @@
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
-sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+sudo apt-get install -y git build-essential dkms gcc linux-headers-$(uname -r) apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
@@ -18,6 +13,5 @@ cd /tmp
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install docker-compose
-sudo apt-get install git build-essential dkms gcc linux-headers-$(uname -r)
 cd ~
 mkdir docker
